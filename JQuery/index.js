b2527f2c63e347e93fbb1a3ce04909c07dbf8663
Sunil-1508 <20a91a05b1@aec.edu.in>
2023-09-09 19:38:50 +0530

    adding to github

---
+++ JQuery/index.js
@@ -0,0 +1,11 @@
+//alert("hello world hi");
+// $(document).ready(function(){
+//     $("h1").css("color","yellow");
+// });
+
+// document.getElementsByTagName("h1")[0].style.color="blue";
+
+
+$("h1").css("color","pink")
+
+$("a").attr("href","https://www.github.com");
