57b5c3a8649fd4ef3d0c5fb5ff9f274c62376a68
Sunil-1508 <20a91a05b1@aec.edu.in>
2023-09-10 20:07:37 +0530

    game completed

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -1,28 +1,88 @@
 const buttonColours =["red" , "blue", "green", "yellow"];
 var gamePattern=[];
+var userClickedPattern=[];
 
+var p=false;
+var level=0;
+
+$(document).keypress(function(){
+    if(p==false){
+        nextSequence();
+        p=true;
+    }
+})
+
+$(".btn").click(function(){
+
+    var userChosenColour=$(this).attr("id");
+
+    userClickedPattern.push(userChosenColour);
+
+    // console.log("User :"+userClickedPattern);
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+    checkAnswer(level);
+
+});
+
+
+function playSound(name){
+    var audio = new Audio("./sounds/"+name+".mp3")
+    audio.play();
+}
+
+function animatePress(currentColour){
+
+    var c=$("#"+currentColour).addClass("pressed");
+    setTimeout(function(){
+        c.removeClass("pressed");
+    },100);
+}
 
 function nextSequence(){
+    
+    $("h1").text("Level "+level);
+    
+
+    var  randomNumber=Math.floor(Math.random()*4);
+
+    var  randomChosenColour=buttonColours[randomNumber]
+
+    gamePattern.push(randomChosenColour);
+
+    // console.log("comp: "+gamePattern);
+
+    $('#'+randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColour);
+    animatePress(randomChosenColour);
+
+    level+=1;
+
+}
 
 
-var  randomNumber=Math.floor(Math.random()*4);
 
-var  randomChosenColour=buttonColours[randomNumber]
+function checkAnswer(currentLevel){
+
+    console.log("User :"+userClickedPattern);
+    console.log("comp: "+gamePattern);
+    console.log("Lev :"+currentLevel);
 
 
-gamePattern.push(randomChosenColour);
+    if(gamePattern[currentLevel-1]==userClickedPattern[currentLevel-1]){
+        console.log("correct");
+        setTimeout(function(){
+            nextSequence();
+        },1000)
+    }
+    else{
+        console.log("wrong");
+        $("h1").text("Game Over");
+        $(".btn").css("backgroundColor","gray");
+        $(document).click(false);
 
-console.log(randomChosenColour);
-
-//document.getElementById(randomChosenColour).style.backgroundColor="pink";
-
-//$('#'+randomChosenColour).css("backgroundColor","pink")
-$(document).ready(()=>{
-    setTimeout(()=>{
-    $('#'+randomChosenColour).fadeOut();
-    $('#'+randomChosenColour).fadeIn();
-   
-    },100);
- });
-
+        setTimeout(function(){
+            window.location.href="./index.html";
+        },2000)
+    }
 }
