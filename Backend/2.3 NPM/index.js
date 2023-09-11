b192df91a6f0d50cade3c9d1467751c2a2b1a0e4
Sunil-1508 <20a91a05b1@aec.edu.in>
2023-09-11 22:37:54 +0530

    QR-Project-Done

--- Backend/2.3 NPM/index.js
+++ Backend/2.3 NPM/index.js
@@ -1,7 +1,19 @@
-var name = require("sillyname")
+//var name = require("sillyname")           // CJS        type="commonjs"
 
-var myname=name()
+import name from 'sillyname';               // ES module  type="module"
+
+import hero from 'superheroes';
+
+
+const myname=name()
 
 console.log(`Hello, my name is ${myname}`)
+
+console.log(`I am ${hero.random()}!`)
+
+
+
+
+
 
 
