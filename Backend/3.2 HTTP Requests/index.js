38816570f05482881fde2d87bb6ac58ee02fbbe7
Sunil-1508 <20a91a05b1@aec.edu.in>
2023-09-12 23:09:13 +0530

    upto GET

--- Backend/3.2 HTTP Requests/index.js
+++ Backend/3.2 HTTP Requests/index.js
@@ -5,8 +5,17 @@ const app=ep()
 const port=3000;
 
 app.get('/',(req,res)=>{
-    res.send("Hello World !!!")
+    res.send("<h1>Hello World !!!</h1>")
+    //console.log(req.rawHeaders)
 })
+
+
+app.get('/about',(req,res)=>{
+    res.send("<h1>This is About page.</h1>")
+   
+})
+
+
 
 app.listen(port,()=>{
     console.log(`Server is running in port ${port}.`)
